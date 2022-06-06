f0d9afd21cff77f2e7349988a16d2318a84e7636
Edifonjack1 <edifonjack@gmail.com>
2022-06-06 15:02:12 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (succes)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcomes is a piece of art,\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
