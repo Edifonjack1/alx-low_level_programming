e9602f29ec2ab1fdcb446b26191f6ef0f428fd4b
Edifonjack1 <edifonjack@gmail.com>
2022-06-08 01:46:42 -0700

    putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/** main - check description
+ *
+ * Return: 0 successfully
+ */
+int main(void)
+{
+	char text[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
